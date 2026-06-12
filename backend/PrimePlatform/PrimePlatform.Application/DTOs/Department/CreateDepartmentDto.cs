f80d45ba7fa0919d7ef86ecb8dc059d49a1d68ac
zoraizM <zoraizmazari@gmail.com>
2026-06-12 03:39:06 +0200

    refactor

---
+++ backend/PrimePlatform/PrimePlatform.Application/DTOs/Department/CreateDepartmentDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimePlatform.Application.DTOs.Department
+{
+    public class CreateDepartmentDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Code { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ backend/PrimePlatform/PrimePlatform.Application/DTOs/Employee/CreateEmployeeDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimePlatform.Application.DTOs.Employee
+{
+    public class CreateEmployeeDto
+    {
+        public string EmployeeCode { get; set; } = string.Empty;
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public DateTime JoiningDate { get; set; }
+
+        public decimal MonthlySalary { get; set; }
+
+        public Guid DepartmentId { get; set; }
+    }
+}
